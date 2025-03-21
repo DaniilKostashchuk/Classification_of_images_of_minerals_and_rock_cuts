9fa1c3c8a63abbbeeafb8bd7f1a246add8962d5f
DaniilKostashchuk <125457617+DaniilKostashchuk@users.noreply.github.com>
2025-03-21 12:00:19 +0700

    Create Transformations_for_images.py

---
+++ Transformations_for_images/Transformations_for_images.py
@@ -0,0 +1,9 @@
+data_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(img_size, scale=(0.8, 1.0)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=15),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1),
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
